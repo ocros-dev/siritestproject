9bce169d2c80a4fa3f98af3d760edc9a67258787
Kuuzo <77974730+Kuuzo@users.noreply.github.com>
2022-06-14 15:37:58 -0400

    Remove commented line

--- src/ios/AppDelegate+SiriShortcuts.h
+++ src/ios/AppDelegate+SiriShortcuts.h
@@ -1,7 +1,7 @@
-/*#import "AppDelegate.h"
+#import "AppDelegate.h"
 
 @interface AppDelegate (siriShortcuts)
     - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler ;
     @property (nonatomic, strong) IBOutlet NSUserActivity* userActivity;
 @end
-*/
+
